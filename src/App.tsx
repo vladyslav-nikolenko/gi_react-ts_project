7a44cb1666f0750e0bc393626a0faa92fea2d691
Vladyslav Nikolenko <v.nikolenko@godeltech.com>
2023-01-11 11:04:12 +0100

    map cardComponent done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './components/main';
 import MyLibrary from './components/myLibrary';
-import { filmsArray } from './mockCards';
 
 function App(): JSX.Element {
+  const [filmsArray, setFilmsArray] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/trending/all/week?api_key=2032130bcff8870a261e91758a5d6ab3',
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+      .then(response => response.json())
+      .then(res => setFilmsArray(res.results))
+      .catch(err => console.log(err));
+  }, []);
   return (
     <BrowserRouter basename="/gi_react-ts_project">
       <Routes>
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Box } from '@mui/material';
 import Footer from './footer';
 import Header from './header';
@@ -8,23 +8,7 @@ import { ICard } from '../models';
 interface MainProps {
   filmsArray: ICard[];
 }
-// https://api.themoviedb.org/3/trending/all/week?api_key=2032130bcff8870a261e91758a5d6ab3&language=en-US
-// https://api.themoviedb.org/3/trending/all/week?api_key=2032130bcff8870a261e91758a5d6ab3&page=2
 function Main({ filmsArray }: MainProps): JSX.Element {
-  useEffect(() => {
-    fetch(
-      'https://api.themoviedb.org/3/trending/all/week?api_key=2032130bcff8870a261e91758a5d6ab3',
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    )
-      .then(response => response.json())
-      .then(res => console.log(res, 'Response'))
-      .catch(err => console.log(err));
-  });
   return (
     <div>
       <Header />
